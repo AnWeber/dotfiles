68142e9f5b912970b27dae014d45d637b7349b95
Andreas Weber <weber.andreas@gmail.com>
2023-11-28 21:47:08 +0100

    fix: jito

--- .dotfiles/zsh/jira.zsh
+++ .dotfiles/zsh/jira.zsh
@@ -1,4 +1,4 @@
 
-alias jito="cmd.exe /c start \"${jira_url}/browse/$(git jira)\" &> /dev/null"
-alias jiv="jira issue view $(git jira) | cat"
-alias jis="jira issue view $(git jira) | cat"
+alias jito='cmd.exe /c start "${jira_url}/browse/$(git jira)" &> /dev/null'
+alias jiv='jira issue view "$(git jira)" | cat'
+alias jis='jira issue view "$(git jira)" | cat'
