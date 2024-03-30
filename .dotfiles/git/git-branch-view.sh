294824faa49aa7ac546d78f55af0ec1164d00b86
Andreas Weber <weber.andreas@gmail.com>
2024-03-30 22:28:19 +0100

    feat: add git extras

--- .dotfiles/git/git-branch-view.sh
+++
@@ -1,1 +0,0 @@
-git branch -r | grep -v HEAD | while read b; do git log --color --format="%ci _%C(magenta)%cr %C(bold cyan)$b%Creset %s %C(bold blue)<%an>%Creset" $b | head -n 1; done | sort -r | cut -d_ -f2- | sed 's;origin/;;g' | head -10
