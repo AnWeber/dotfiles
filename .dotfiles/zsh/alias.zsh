f54620a8199e131cf16fd93eacfc2b0941c60763
Andreas Weber <weber.andreas@gmail.com>
2024-05-06 20:34:23 +0200

    feat: update alias

--- .dotfiles/zsh/alias.zsh
+++ .dotfiles/zsh/alias.zsh
@@ -1,2 +1,8 @@
-alias ll="eza -l"
-alias la="eza -la"
+alias la='eza -la --no-user'   #long list,show almost all,show type,human readable
+alias ll='eza -l --no-user'      #long list
+
+alias grep='grep --color'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
