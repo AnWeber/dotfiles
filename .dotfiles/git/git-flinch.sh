d6cf22140743a222beb76f380eb16691040f5d0c
Andreas Weber <weber.andreas@gmail.com>
2025-01-07 09:33:16 +0100

    feat: update git flinch

--- .dotfiles/git/git-flinch.sh
+++ .dotfiles/git/git-flinch.sh
@@ -1,5 +1,15 @@
 
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+      --no-switch) noSwitch=1 ;;
+  esac
+  shift
+done
+
 git fetch --prune
-git dev
+if [ -z "$noSwitch" ]; then
+  git dev
+fi
 git pull
 git branch-clean "$@"
