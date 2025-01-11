2b2955c1bba421eea3cc65fd0bb6f30e1cba0d61
Andreas Weber <weber.andreas@gmail.com>
2025-01-11 17:00:50 +0100

    feat: update whid

--- .dotfiles/git/git-whid.sh
+++ .dotfiles/git/git-whid.sh
@@ -7,4 +7,4 @@ days=$1
 if  [[ $1 == '' ]]; then
  days=7
 fi
-git log --color --author="$user" --graph --since=-"$days days" --pretty=format:'%C(blue)%cs%Creset %C(magenta)|%Creset %s'
+git log --color --author="$user" --graph --since=-"$days days" --pretty=format:"%C(blue)%cs%Creset %C(magenta)|%Creset %s"
