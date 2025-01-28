c100ecc941f3162ff1a920c25bba124e7d72199f
Andreas Weber <weber.andreas@gmail.com>
2025-01-28 11:29:40 +0100

    feat: revert .zshrc changes after install

--- .dotfiles/install/apps/zsh.sh
+++ .dotfiles/install/apps/zsh.sh
@@ -7,6 +7,7 @@ fi
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  cat ~/.zshrc.pre-oh-my-zsh >> ~/.zshrc && source ~/.zshrc
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
